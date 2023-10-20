3c0b89969cabf936f1f5810a01b34f355502e374
Rafael Marcos <rafix995@gmail.com>
2023-10-20 17:18:45 +0100

    Header

--- app/view/index.php
+++ app/view/index.php
@@ -1,3 +1,65 @@
-<?php
-    echo "hello world";
-?>
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>Otaku Oo</title>
+        <meta charset="utf8">
+        <meta name="description" content="A forum from otakus to otakus - Join us">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="support/css/landing_page.css"/>
+    </head>
+
+    <body class="grid">
+        <header class="header">
+            <h1 class="site-title">Otaku Oo</h1>
+            <nav class="nav"> 
+                <ul class="link-list">
+                        <li><a class="link" href="#">Sign up</a></li>
+                        <li><a class="link" href="#">Sign in</a></li>
+                </ul>    
+            </nav>
+        </header>
+
+        <section class="main-section">
+            <div class="main-description">
+                <h1>Create topics and discuss</h1>
+                <p>Join people that like and consume the same as you!</p>
+            <div>
+        </section>
+
+        <section>
+            <div>
+                <h1>Expose your ideas</h1>
+                <p>Tell everyone your opinion about your favorite animes, mangas, manhwas and many more</p>
+            </div>
+            <img src="#" alt="Expose your ideas image">
+        </section>
+
+        <section>
+            <div>
+                <h1>Share any media</h1>
+                <p>An image tells more than a thousand words, what about videos and gifs?</p>
+            </div>
+            <img src="#" alt="Share any media image">
+        </section>
+
+        <section>
+            <div>
+                <h1>Share your posts over the internet</h1>
+                <p>This is your oportunity to attract more traffic on your posts</p>
+            </div>
+            <img src="#" alt="Expose your ideas example">
+        </section>
+
+        <section>
+            <div>
+                <h1>Save what you liked</h1>
+                <p>Favorite everything you want to see later</p>
+            </div>
+            <img src="#" alt="Expose your ideas example">
+        </section>
+
+        <footer>
+            <small>Copyright &copy; Rafael Marcos</small>
+        </footer>
+    </body>
+</html>
