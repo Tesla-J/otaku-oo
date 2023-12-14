254d34066ebe50378e7a36c136b98cdbad85297b
Rafael Marcos <rafix995@gmail.com>
2023-12-14 20:36:12 +0100

    Signin and signup forms stylization

--- app/view/signin.php
+++ app/view/signin.php
@@ -1,25 +1,35 @@
 <html>
 <head>
-    <title>Otaku king - sign up</title>
+    <title>Otaku king - sign in</title>
     <meta charset="utf-8" />
+    <meta name="description" content="A forum from otakus to otakus - Join us">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/view/support/css/global.css" />
+    <link rel="stylesheet" href="/view/support/css/landing_page.css" />
+    <link rel="stylesheet" href="/view/support/css/form.css" />
 </head>
 
 <body>
-    
-    <h1>sign in</h1>
-    <form action="signin" method="post">
-
-        <label for="username">Username:</label>
-        <input id="username" name="username" required/>
-        <br>
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password" required/>
-        <br>
-        <input type="submit" value="Sign in"/>
-        <input type="reset" value="Clear" />
-        <br>
-        <p>Don't have an account? <a href="signup">Sign up</a></p>
-    </form>
+    <section class="wrapper">
+        <h1>Sign in</h1>
+        <form class="form" action="signin" method="post">
+            <div class="field-area">
+                <input class="field" id="username" name="username" placeholder="" autocomplete="off" required/>
+                <label class="label" for="username">Username</label>
+            </div>
+            <br>
+            <div class="field-area">
+                <input class="field" id="password" name="password" placeholder="" type="password" required/>
+                <label class="label" for="password">Password</label>
+            </div>
+            <br>
+            <input class="button" type="submit" value="Sign in"/>
+            <br>
+            <input class="button" type="reset" value="Clear" />
+            <br>
+            <p>Don't have an account? <a class="raw-link" href="signup">Sign up</a></p>
+        </form>
+    </section>
 </body>
 
 </html>
--- app/view/signup.php
+++ app/view/signup.php
@@ -2,29 +2,44 @@
 <head>
     <title>Otaku king - sign up</title>
     <meta charset="utf-8" />
+    <meta name="description" content="A forum from otakus to otakus - Join us">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/view/support/css/global.css" />
+    <link rel="stylesheet" href="/view/support/css/landing_page.css" />
+    <link rel="stylesheet" href="/view/support/css/form.css" />
 </head>
 
 <body>
-    <h1>sign up</h1>
-    <form action="signup" method="post">
-
-        <label for="username">Username:</label>
-        <input id="username" name="username" required/>
-        <br>
-        <label for="email">E-mail:</label>
-        <input id="email" name="email" type="email" />
-        <br>
-        <label for="password1">Password</label>
-        <input id="password1" name="password1" type="password" required/>
-        <br>
-        <label for="password2">Confirm password:</label>
-        <input id="password2" name="password2" type="password" required/>
-        <br>
-        <input type="submit" value="Sign up"/>
-        <input type="reset" value="Clear" />
-        <br>
-        <p>Already have an account? <a href="signin">Sign in</a></p>
-    </form>
+    <section class="wrapper">
+        <h1>Sign up</h1>
+        <form class="form" action="signup" method="post">
+            <div class="field-area">
+                <input class="field" id="username" name="username" placeholder="" required/>
+                <label class="label" for="username">Username</label>
+            </div>
+            <br>
+            <div class="field-area">
+                <input class="field" id="email" name="email" type="email" placeholder="" required />
+                <label class="label" for="email">E-mail:</label>
+            </div>
+            <br>
+            <div class="field-area">
+                <input class="field" id="password1" name="password1" type="password" placeholder="" required/>
+                <label class="label" for="password1">Password</label>
+            </div>
+            <br>
+            <div class="field-area">
+                <input class="field" id="password2" name="password2" type="password" placeholder="" required/>
+                <label class="label" for="password2">Confirm password</label>
+            </div>
+            <br>
+            <input class="button" type="submit" value="Sign up"/>
+            <br>
+            <input class="button" type="reset" value="Clear" />
+            <br>
+            <p>Already have an account? <a class="raw-link" href="signin">Sign in</a></p>
+        </form>
+    </section>
 </body>
 
 </html>
