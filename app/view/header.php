751409fee21e50b762075a8caa252036f6a45249
Rafael Marcos <rafix995@gmail.com>
2023-12-14 20:26:17 +0100

    header will show signout button if user has a session started

--- app/view/header.php
+++ app/view/header.php
@@ -2,8 +2,12 @@
     <h1 class="site-title">Otaku Oo</h1>
     <nav class="nav"> 
         <ul class="link-list">
+                <? if (!isset($_SESSION['username'])): ?>
                 <li><a class="link" href="user/signup">Sign up</a></li>
                 <li><a class="link" href="user/signin">Sign in</a></li>
+                <? else: ?>
+                <li><a class="link" href="/user/signout">Sign out</a></li>
+                <? endif ?>
         </ul>    
     </nav>
 </header>
